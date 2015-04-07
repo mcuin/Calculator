9c50246ceab16225127773adbb5a7b5e6b5e8474
Mykal Cuin <mykcuin@gmail.com>
2015-04-07 12:50:55 -0600

    Testing out different layouts for responsive design of buttons. Added the add methods barebones for ints and doubles.

--- MyApplication/app/src/main/java/com/mykal/memorycalculator/MemoryCalculator.java
+++ MyApplication/app/src/main/java/com/mykal/memorycalculator/MemoryCalculator.java
@@ -30,11 +30,16 @@ public class MemoryCalculator extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public int add(int a, int b) {
+
+    }
+
+    public double add(double a, double b) {
+        
     }
 }
